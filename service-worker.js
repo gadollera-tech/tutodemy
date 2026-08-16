3a93709c47655952bccc6324ab6f5a00012b8ecf
gadollera-tech <gadollera@gmail.com>
2026-08-16 20:15:12 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tutodemy-20260816-paymongo2";
+const CACHE_VERSION = "tutodemy-20260816-professional1";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const CORE_ASSETS = [
@@ -6,19 +6,19 @@ const CORE_ASSETS = [
   "./index.html",
   "./offline.html",
   "./manifest.webmanifest",
-  "./css/style.css?v=20260816-paymongo2",
-  "./js/marketplace-api.js?v=20260816-paymongo2",
+  "./css/style.css?v=20260816-professional1",
+  "./js/marketplace-api.js?v=20260816-professional1",
   "./js/captcha.js?v=20260816-ui10",
   "./js/home-auth.js?v=20260816-ui10",
-  "./js/main.js?v=20260816-webpush2",
+  "./js/main.js?v=20260816-professional1",
   "./tutor-dashboard.html",
   "./tutor-onboarding.html",
-  "./js/tutor-dashboard.js?v=20260816-workspacefix2",
-  "./js/tutor-onboarding.js?v=20260809-tutorfix1",
+  "./js/tutor-dashboard.js?v=20260816-professional1",
+  "./js/tutor-onboarding.js?v=20260816-professional1",
   "./admin.html",
   "./dashboard.html",
-  "./js/dashboard.js?v=20260816-podium2",
-  "./js/admin.js?v=20260816-paymongo2",
+  "./js/dashboard.js?v=20260816-professional1",
+  "./js/admin.js?v=20260816-professional1",
   "./js/notifications.js?v=20260816-webpush2",
   "./js/pwa.js?v=20260809-pwa1",
   "./assets/images/icon-192.png",
