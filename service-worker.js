f8b53aee72d76ae0a85bd9568d9ac41ccea7dc79
gadollera-tech <gadollera@gmail.com>
2026-08-16 20:30:08 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tutodemy-20260816-professional1";
+const CACHE_VERSION = "tutodemy-20260816-landinghero1";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const CORE_ASSETS = [
@@ -6,7 +6,7 @@ const CORE_ASSETS = [
   "./index.html",
   "./offline.html",
   "./manifest.webmanifest",
-  "./css/style.css?v=20260816-professional1",
+  "./css/style.css?v=20260816-landinghero1",
   "./js/marketplace-api.js?v=20260816-professional1",
   "./js/captcha.js?v=20260816-ui10",
   "./js/home-auth.js?v=20260816-ui10",
