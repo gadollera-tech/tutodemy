03b57473c79bbdb93fee54e8fef7874a470b6fd3
gadollera-tech <gadollera@gmail.com>
2026-08-23 20:09:26 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tutodemy-20260823-reasoningvideos1";
+const CACHE_VERSION = "tutodemy-20260823-mathmodules1";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const CORE_ASSETS = [
