cf724a9d09f81d4fa8ebdaca8681b92cd0b4a142
gadollera-tech <gadollera@gmail.com>
2026-08-23 17:39:14 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tutodemy-20260823-homeheader1";
+const CACHE_VERSION = "tutodemy-20260823-homeheader2";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const CORE_ASSETS = [
@@ -6,8 +6,8 @@ const CORE_ASSETS = [
   "./index.html",
   "./offline.html",
   "./manifest.webmanifest",
-  "./css/style.css?v=20260823-homeheader1",
-  "./js/main.js?v=20260823-homeheader1",
+  "./css/style.css?v=20260823-homeheader2",
+  "./js/main.js?v=20260823-homeheader2",
   "./js/pwa.js?v=20260809-pwa1",
   "./assets/images/icon-192.png",
   "./assets/images/apple-touch-icon.png",
